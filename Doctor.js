46b1a2468d70fd7ad167b0e8b13d33b1d6c20081
mavis acheampong <amavis32@yahoo.com>
2020-07-14 14:58:08 -0400

    finished assessment

---
+++ Doctor.js
@@ -0,0 +1,11 @@
+class Doctor extends Traveler {
+    constructor(name) {
+        super(name)
+
+    }
+
+    heal(traveler) {
+        traveler.isHealthy = true
+
+    }
+}
---
+++ Hunter.js
@@ -0,0 +1,26 @@
+class Hunter extends Traveler {
+    constructor(name) {
+        super(name)
+        this.food = 2
+    }
+
+    hunt() {
+        this.food += 5
+    }
+
+    eat() {
+        if (this.food < 2) {
+            this.isHealthy = false
+            this.food = 0
+        } else {
+            this.food -= 2
+        }
+    }
+
+    giveFood(traveler, numOfFoodUnits) {
+        if (this.food >= numOfFoodUnits) {
+            this.food -= numOfFoodUnits
+            traveler.food += numOfFoodUnits
+        }
+    }
+}
